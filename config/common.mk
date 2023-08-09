44f2e8c59b26eb52018b4dedf558ead50b1006ad
jhenrique09 <jhsv09@gmail.com>
2023-08-09 22:29:23 +0000

    vendor: Drop SimpleDeviceConfig
    Change-Id: Iba7e9e042b037f1147647608de9ab03a21fa00f9
    Signed-off-by: Prabhat Maurya <prabhat@e.email>

--- config/common.mk
+++ config/common.mk
@@ -143,12 +143,7 @@ PRODUCT_COPY_FILES += \
     vendor/blaze/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 endif
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleDeviceConfig \
-    SimpleSettingsConfig
-
-# Extra tools in Lineage
+# Extra tools in x01
 PRODUCT_PACKAGES += \
     bash \
     curl \
