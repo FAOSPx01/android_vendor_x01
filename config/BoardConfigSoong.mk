71a3299136d55a6d5c7df0622abc9783c71c6cc9
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-11 16:04:47 +0000

    soong: Add TARGET_USES_OPLUS_CAMERA
    Change-Id: Ifb3ff3a1141712a255640676d24b018ae516add8
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Prabhat Maurya <prabhat@e.email>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -55,7 +55,8 @@ SOONG_CONFIG_x01GlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    uses_oplus_camera
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
 SOONG_CONFIG_x01NvidiaVars += \
@@ -85,6 +86,7 @@ SOONG_CONFIG_x01QcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR
 SOONG_CONFIG_x01QcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_x01QcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_x01GlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_x01GlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
