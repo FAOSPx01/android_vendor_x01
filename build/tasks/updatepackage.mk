ecdc7873eb53c19e35362f4b5fb41013f30c5ff0
PainKiller3 <ninadpatil100@gmail.com>
2024-04-07 17:06:47 +0800

    vendor: overlay: Import pinner configuration from AOSPA
    Change-Id: I94392e2acd624a64153978fd9b1dc7846a873243
    Co-authored-by: xboxfanj <jake@aospa.co>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: Prabhat Maurya <prabhat@e.email>

--- build/tasks/updatepackage.mk
+++ build/tasks/updatepackage.mk
@@ -15,14 +15,14 @@
 # -----------------------------------------------------------------
 # SuperiorOS OTA update package
 
-BLAZE_TARGET_UPDATEPACKAGE := $(PRODUCT_OUT)/$(BLAZE_VERSION)-updateimages.zip
+X01_TARGET_UPDATEPACKAGE := $(PRODUCT_OUT)/$(X01_VERSION)-updateimages.zip
 
 .PHONY: updatepackage
 updatepackage: $(INTERNAL_UPDATE_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(BLAZE_TARGET_UPDATEPACKAGE)
+	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(X01_TARGET_UPDATEPACKAGE)
 	@echo ""
 	@echo ""
 	@echo "**************************************************"
-	@echo " OTA update package is ready: $(BLAZE_TARGET_UPDATEPACKAGE)"
+	@echo " OTA update package is ready: $(X01_TARGET_UPDATEPACKAGE)"
 	@echo "**************************************************"
 	@echo ""
