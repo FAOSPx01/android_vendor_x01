c3d8df8c84617867a198ba0a5d839a85f4265d28
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 15:45:31 +0200

    config: Rename mobile-specific configs
    Since common_full.mk and common_mini.mk are mobile-specific, rename them
    
    Change-Id: I2c47fd0688350ac815ccecbff7b453d7fab5d5ea
    Signed-off-by: Prabhat Maurya <prabhat@e.email>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,14 @@
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/x01/config/common_mobile_full.mk)
 
+# Required packages
+PRODUCT_PACKAGES += \
+    LatinIME
+
+# Include blaze LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/x01/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/x01/overlay/dictionaries
+
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true
