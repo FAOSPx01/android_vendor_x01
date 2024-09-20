ProjectBlaze/vendor_blaze

vendor: Include packages.mk
*That's why GameSpace wasn't getting builded :(

Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/common.mk
@@ -289,3 +289,4 @@ PRODUCT_PACKAGES += \
     BlazeWallpaperStub
 
 include vendor/blaze/config/pixel_props.mk
+include vendor/blaze/config/packages.mk

FAOSPx01/android_vendor_x01

Signed-off-by: Prabhat Maurya <prabhat@e.email>

config/common.mk
@@ -286,3 +286,4 @@ include vendor/x01/config/version.mk
 -include vendor/x01/config/partner_gms.mk
 
 include vendor/x01/config/pixel_props.mk
+include vendor/x01/config/packages.mk