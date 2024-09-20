zaidkhan0997/vendor_blaze

vendor: soong: Add camera_needs_client_info_lib_oplus
Change-Id: I075cdcae3b74104ba6923c151b1e92e4513e8996

config/BoardConfigSoong.mk
@@ -95,6 +95,7 @@ SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_T
 SOONG_CONFIG_blazeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

FAOSPx01/android_vendor_x01

Signed-off-by: Prabhat Maurya <prabhat@e.email>

config/BoardConfigSoong.mk
@@ -100,6 +100,7 @@ SOONG_CONFIG_x01GlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIM
 SOONG_CONFIG_x01GlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_x01GlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_x01GlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_x01GlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0