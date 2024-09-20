ProjectBlaze/vendor_blaze

soong: introduce TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY
Set TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY to the key code to skip in InputDispatcher

Signed-off-by: daniml3 <danimoral1001@gmail.com>
Signed-off-by: baolong24 <1667375308@qq.com>
Change-Id: Ie39b0c210519fccdcc9509b567f839902c80f248
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/BoardConfigSoong.mk
@@ -45,6 +45,7 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_health_charging_control_supports_deadline \
     target_health_charging_control_supports_toggle \
     target_init_vendor_lib \
+    target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
     target_trust_usb_control_path \
@@ -87,6 +88,7 @@ SOONG_CONFIG_blazeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGE
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
+TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
 TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
@@ -117,6 +119,7 @@ SOONG_CONFIG_blazeGlobalVars_target_health_charging_control_supports_bypass := $
 SOONG_CONFIG_blazeGlobalVars_target_health_charging_control_supports_deadline := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE)
 SOONG_CONFIG_blazeGlobalVars_target_health_charging_control_supports_toggle := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE)
 SOONG_CONFIG_blazeGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_blazeGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 SOONG_CONFIG_blazeGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_blazeGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_blazeGlobalVars_target_trust_usb_control_path := $(TARGET_TRUST_USB_CONTROL_PATH)

FAOSPx01/android_vendor_x01

Signed-off-by: Prabhat Maurya <prabhat@e.email>

config/BoardConfigSoong.mk
@@ -49,6 +49,7 @@ SOONG_CONFIG_x01GlobalVars += \
     target_health_charging_control_supports_deadline \
     target_health_charging_control_supports_toggle \
     target_init_vendor_lib \
+    target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
     target_power_libperfmgr_mode_extension_lib \
     target_powershare_path \
@@ -95,6 +96,7 @@ SOONG_CONFIG_x01GlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
+TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
 TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
@@ -139,6 +141,7 @@ SOONG_CONFIG_x01GlobalVars_target_trust_usb_control_enable := $(TARGET_TRUST_USB
 SOONG_CONFIG_x01GlobalVars_target_trust_usb_control_disable := $(TARGET_TRUST_USB_CONTROL_DISABLE)
 SOONG_CONFIG_x01GlobalVars_disable_bluetooth_le_read_buffer_size_v2 := $(TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2)
 SOONG_CONFIG_x01GlobalVars_disable_bluetooth_le_set_host_feature := $(TARGET_DISABLE_BLUETOOTH_LE_SET_HOST_FEATURE)
+SOONG_CONFIG_x01GlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_x01QcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display