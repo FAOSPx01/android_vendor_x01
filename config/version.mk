zaidkhan0997/vendor_blaze

[SQUASH] config: Enable inline sigining for user and userdebug build
config: Conditionally enable dev keys signing

Signed-off-by: Dmitrii <bankersenator@gmail.com>

config/version: PRODUCT_VERITY_SIGNING_KEY is deprecated

avb built on older android with same key will still flash and run just
fine locked on 14.

TODO: consider dropping the deprecation of VB1.0 in build
Signed-off-by: Dmitrii <bankersenator@gmail.com>

signing: support inline signing for U [2/2]

For those who have used .android-certs previously, cp .android-certs/* vendor/voltage/signing/keys/

Signed-off-by: Dmitrii <bankersenator@gmail.com>

version: Sign every build variant but eng

Signed-off-by: Dmitrii <bankersenator@gmail.com>

Co-authored-by: RealJohnGalt <RealJohnGalt@users.noreply.github.com>

config/version.mk
@@ -58,3 +58,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.blaze.device=$(BLAZE_BUILD) \
   org.blaze.version=$(BLAZEVERSION) \
   ro.blaze.maintainer=$(BLAZE_MAINTAINER)
+
+# Sign Build
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/blaze/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/blaze/signing/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard vendor/blaze/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/blaze/signing/keys/otakey.x509.pem
+endif
+endif

FAOSPx01/android_vendor_x01

Signed-off-by: Prabhat Maurya <prabhat@e.email>

config/version.mk
@@ -69,3 +69,14 @@ ifneq (,$(wildcard vendor/x01/signing/keys/otakey.x509.pem))
 PRODUCT_OTA_PUBLIC_KEYS := vendor/x01/signing/keys/otakey.x509.pem
 endif
 endif
+
+# Sign Build
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/x01/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/x01/signing/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard vendor/x01/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/x01/signing/keys/otakey.x509.pem
+endif
+endif