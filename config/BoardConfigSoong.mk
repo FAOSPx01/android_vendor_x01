ProjectBlaze/vendor_blaze

vendor: Allow using legacy fd
Change-Id: I88e355e60de63a7f81adf8e25fabbd305fb0af7c

config/BoardConfigSoong.mk
@@ -46,7 +46,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_legacy_fd_fbdev
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -70,6 +71,7 @@ SOONG_CONFIG_blazeGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY
 SOONG_CONFIG_blazeNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_blazeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blazeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_blazeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

FAOSPx01/android_vendor_x01

Signed-off-by: itsxrp <itsxrproms@gmail.com>
Signed-off-by: Prabhat Maurya <prabhat@e.email>

config/BoardConfigSoong.mk
@@ -52,7 +52,8 @@ SOONG_CONFIG_x01GlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_legacy_fd_fbdev
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
 SOONG_CONFIG_x01NvidiaVars += \
@@ -81,6 +82,7 @@ SOONG_CONFIG_x01NvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_EN
 SOONG_CONFIG_x01QcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_x01QcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_x01QcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_x01GlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0