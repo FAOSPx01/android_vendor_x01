zaidkhan0997/vendor_blaze

soong: Add TARGET_USES_NOTHING_CAMERA
Change-Id: Ib03ed29f9649750fa0f10f28615c109ded21b55a
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
Signed-off-by: aswin7469 <aswinas@pixysos.com>
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -57,7 +57,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    camera_needs_client_info
+    camera_needs_client_info \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -93,6 +94,7 @@ SOONG_CONFIG_blazeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGE
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

FAOSPx01/android_vendor_x01

Signed-off-by: Prabhat Maurya <prabhat@e.email>

config/BoardConfigSoong.mk
@@ -62,7 +62,8 @@ SOONG_CONFIG_x01GlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    camera_needs_client_info
+    camera_needs_client_info \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
 SOONG_CONFIG_x01NvidiaVars += \
@@ -98,6 +99,7 @@ SOONG_CONFIG_x01GlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS
 SOONG_CONFIG_x01GlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_x01GlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_x01GlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_x01GlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0